03e90b4f5ba9b39c634fa9197770a3a207df6bf6
evan82 <eduffield82@gmail.com>
2016-04-19 12:16:37 -0700

    Update 001.sql

--- database/001.sql
+++ database/001.sql
@@ -46,8 +46,6 @@ company_employees
   from_id
   to_id
 
-
-
 company_proposals
 create
 create_event
@@ -55,21 +53,4 @@ groups
 group_member
 users
 user_member
-transactions
-
-CREATE TABLE `governance_object` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `parent_id` int(11) DEFAULT NULL,
-  `hash` varchar(255) DEFAULT NULL,
-  `parent_hash` varchar(255) DEFAULT NULL,
-  `name` varchar(64) DEFAULT NULL,
-  `start_time` bigint(11) DEFAULT NULL,
-  `end_time` bigint(11) DEFAULT NULL,
-  `priority` int(20) DEFAULT NULL,
-  `type_version` int(20) DEFAULT NULL,
-  `revision` int(20) DEFAULT NULL,
-  `owner_address` varchar(255) DEFAULT NULL,
-  `fee_tx` varchar(255) DEFAULT NULL,
-  `registers` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+transaction
