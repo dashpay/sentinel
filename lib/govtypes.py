baebf94cf6d0bf5b6b1e7ab4e269732c792af7d1
Evan Duffield <evan@dashpay.io>
2016-05-17 13:41:43 -0700

    Add govtypes for payments, move primatives to 1000s

--- lib/govtypes.py
+++ lib/govtypes.py
@@ -2,10 +2,11 @@
 # Name the types of governance objects here, dash is agnostict
 
 contract = 1
+payment = 2
 
 """
-user = 2
-employee = 3
-manager = 4
-project = 5
+user = 1001
+employee = 1002
+manager = 1003
+project = 1004
 """
