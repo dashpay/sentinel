5b949326da12cdc47cdeb1e694eb2d183f5594f2
Nathan Marley <nathan.marley@gmail.com>
2016-08-19 16:46:32 -0500

    update test sample for config

--- test_sample.py
+++ test_sample.py
@@ -7,10 +7,10 @@ def test_answer():
     assert func(3) == 5
 
 def test_config():
-    assert config.hostname == '127.0.0.1'
-    assert config.username == 'dashdrive'
-    assert config.password == 'dashdrive'
-    assert config.database == 'sentinel'
-    assert config.dash_cli == '/Users/nmarley/Dash/code/dash/src/dash-cli'
-    assert config.datadir == '/Users/nmarley/Library/Application Support/DashCore'
+    assert config.db_config['hostname'] == '127.0.0.1'
+    assert config.db_config['username'] == 'dashdrive'
+    assert config.db_config['password'] == 'dashdrive'
+    assert config.db_config['database'] == 'sentinel'
 
+    # assert config.dash_cli == '/Users/nmarley/Dash/code/dash/src/dash-cli'
+    # assert config.datadir == '/Users/nmarley/Library/Application Support/DashCore'
