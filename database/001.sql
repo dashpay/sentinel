72f495ad77607ce7c955fcf445c6d4f429647fef
Nathan Marley <nathan.marley@gmail.com>
2016-08-20 22:16:55 -0500

    unique names for indexes

--- database/001.sql
+++ database/001.sql
@@ -35,7 +35,7 @@ CREATE TABLE `proposal` (
   `payment_address` varchar(255) DEFAULT NULL,
   `payment_amount` decimal(16,8) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `index2` (`governance_object_id`)
+  UNIQUE KEY `index_proposal_governance_object_id` (`governance_object_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE superblock (
@@ -46,7 +46,7 @@ CREATE TABLE superblock (
   `payment_addresses` text,
   `payment_amounts` text,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `index2` (`governance_object_id`)
+  UNIQUE KEY `index_superblock_governance_object_id` (`governance_object_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `event` (
@@ -58,7 +58,7 @@ CREATE TABLE `event` (
   `error_time` int(11) NOT NULL DEFAULT '0',
   `error_message` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `governance_object_id` (`governance_object_id`)
+  UNIQUE KEY `index_event_governance_object_id` (`governance_object_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `action` (
