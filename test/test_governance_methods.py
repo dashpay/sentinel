b3ac044ed916f5d62e2169f0dd0b4adeb030fe02
Nathan Marley <nathan.marley@gmail.com>
2016-08-22 14:22:52 -0500

    TEMP: testing data

--- test/test_governance_methods.py
+++ test/test_governance_methods.py
@@ -39,6 +39,11 @@ def governance_object():
   return govobj
 
 
+# proposal -c -p chrono-trigger-party -d https://dashcentral.com/chrono-trigger-party -s 2016/09/01 -e 2016/10/01 -x yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui -a 100
+# superblock -c -p yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui=5,yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV=3 -b 523000
+        # govobj = GovernanceObject()
+        # govobj.load(pw_event.governance_object_id)
+
 def test_prepare_command(governance_object):
   d = governance_object.governance_object.get_dict()
   assert type(d) == type({})
