ca4df469be5f7c7502cf81b4d40e24f775a54671
Nathan Marley <nathan.marley@gmail.com>
2016-08-22 15:35:18 -0500

    notes/tests

--- test_sample.py
+++ test_sample.py
@@ -7,10 +7,11 @@ def test_answer():
     assert func(3) == 5
 
 def test_config():
-    assert config.db_config['hostname'] == '127.0.0.1'
-    assert config.db_config['username'] == 'dashdrive'
-    assert config.db_config['password'] == 'dashdrive'
-    assert config.db_config['database'] == 'sentinel'
+    test_db_cfg = config.db['production']
+    assert test_db_cfg['host'] == '127.0.0.1'
+    assert test_db_cfg['user'] == 'dashdrive'
+    assert test_db_cfg['passwd'] == 'dashdrive'
+    assert test_db_cfg['database'] == 'sentinel'
 
     # assert config.dash_cli == '/Users/nmarley/Dash/code/dash/src/dash-cli'
     # assert config.datadir == '/Users/nmarley/Library/Application Support/DashCore'
