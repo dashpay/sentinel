908b79382b0fbd23634afefa5f79a79eb3dc99e3
Nathan Marley <nathan.marley@gmail.com>
2016-08-23 12:24:53 -0500

    tweak test for govobj prepare command

--- test/test_governance_methods.py
+++ test/test_governance_methods.py
@@ -4,46 +4,52 @@ import os
 os.environ['SENTINEL_ENV'] = 'test'
 import sys
 sys.path.append( os.path.join( os.path.dirname(__file__), '..', 'lib' ) )
+import re
 
 # NGM/TODO: setup both Proposal and Superblock, and insert related rows,
 # including Events
 
 def setup():
-    pass
+    from models import PeeWeeGovernanceObject, PeeWeeProposal, PeeWeeEvent
+
+    # clear tables first...
+    PeeWeeEvent.delete().execute()
+    PeeWeeProposal.delete().execute()
+    PeeWeeGovernanceObject.delete().execute()
 
 def teardown():
     pass
 
 
-# @pytest.fixture
-# def proposal_name():
-#     return "chrono-trigger-party"
+@pytest.fixture
+def proposal():
+    from models import PeeWeeProposal
 
-# pw_event = PeeWeeEvent.get(
-# (PeeWeeEvent.start_time < misc.get_epoch() ) &
-# (PeeWeeEvent.error_time == 0) &
-# (PeeWeeEvent.prepare_time == 0)
-# )
-#
-# if pw_event:
-# govobj = GovernanceObject()
-# govobj.load(pw_event.governance_object_id)
+    # NOTE: no governance_object_id is set
+    pobj = PeeWeeProposal(
+        start_epoch     = 1483250400,  # 2017-01-01
+        end_epoch       = 1491022800,  # 2017-04-01
+        proposal_name   = "chrono-trigger-party",
+        description_url = "https://dashcentral.com/chrono-trigger-party",
+        payment_address = "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui",
+        payment_amount  = 7
+    )
 
-# Event model
+    # NOTE: this object is (intentionally) not saved yet.
+    #       We want to return an built, but unsaved, object
+    return pobj
 
-#govobj.get_prepare_command
 
 # GovernanceObject model
 @pytest.fixture
-def governance_object():
-  from models import PeeWeeGovernanceObject
-  from governance import GovernanceObject
-  govobj = GovernanceObject()
-  govobj.init(object_name = "chrono-trigger-party", object_creation_time = 1471899315)
-  govobj.save()
+def governance_object(**kwargs):
+    from models import PeeWeeGovernanceObject
+    from governance import GovernanceObject
+    govobj = GovernanceObject()
+    govobj.init(**kwargs)
+    # NOTE: do not save, return an unsaved govobj
 
-  return govobj
-
+    return govobj
 
 # Starting with this info...
 #
@@ -57,36 +63,49 @@ def governance_object():
 
 
 def test_prepare_command(governance_object):
-  from models import PeeWeeProposal
+    prop = proposal()
+    outer_go = governance_object
 
-  #d = governance_object.governance_object.get_dict()
-  #assert type(d) == type({})
+    # governance_object_id
 
-  go = governance_object
-  goid = go.governance_object.id
-  proposal_name = "chrono-trigger-party"
+    # some small manipulations for our test cases
+    outer_go.governance_object.save()
+    outer_go.governance_object.id = 5
+    outer_go.governance_object.object_creation_time = 1471898632
+    outer_go.governance_object.save()
 
-  description_url = "https://dashcentral.com/chrono-trigger-party"
-  payment_address = "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui"
-  payment_amount = 7
+    outer_go.governance_object.object_name = prop.proposal_name
+    prop.governance_object = outer_go.governance_object
 
-  pw_proposal = PeeWeeProposal(
-    start_epoch = 1472706000,
-    end_epoch = 1475298000,
-    # governance_object_id = goid,
-    governance_object_id = 5,
-    proposal_name = proposal_name,
-    description_url = description_url,
-    payment_address = payment_address,
-    payment_amount = payment_amount
-  )
-  go.add_subclass("proposal", pw_proposal)
-  go.compile_subclasses()
-  # go.save()
-  # pw_proposal.save()
+    try:
+        with outer_go.governance_object._meta.database.atomic():
+            outer_go.governance_object.save()
+            prop.save()
+    except:
+        print "Pork Chop Sandwiches!!"
 
 
-  # gobject_command = "gobject prepare 0 1 1471898632 %s 5b5b2270726f706f73616c222c207b22656e645f65706f6368223a20313437353239383030302c2022676f7665726e616e63655f6f626a6563745f6964223a20352c20227061796d656e745f61646472657373223a2022795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e74223a20372c202270726f706f73616c5f6e616d65223a20226368726f6e6f2d747269676765722d7061727479222c202273746172745f65706f6368223a20313437323730363030307d5d5d" % proposal_name
-  gobject_command = "gobject prepare 0 1 1471899315 chrono-trigger-party 5b5b2270726f706f73616c222c207b22656e645f65706f6368223a20313437353239383030302c2022676f7665726e616e63655f6f626a6563745f6964223a20352c20227061796d656e745f61646472657373223a2022795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e74223a20372c202270726f706f73616c5f6e616d65223a20226368726f6e6f2d747269676765722d7061727479222c202273746172745f65706f6368223a20313437323730363030307d5d5d"
+    # TODO: ensure correct # of fields (without double-spaces, which indicate a
+    # missing field)
+    #
+    # gobject prepare 0 1 1471970731 chrono-trigger-party 5b5b2270726f706f73616c222c207b22656e645f65706f6368223a20313439313032323830302c2022676f7665726e616e63655f6f626a6563745f6964223a20342c20227061796d656e745f61646472657373223a2022795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e74223a20372e30303030303030302c202270726f706f73616c5f6e616d65223a20226368726f6e6f2d747269676765722d7061727479222c202273746172745f65706f6368223a20313438333235303430307d5d5d
 
-  assert go.get_prepare_command() == gobject_command
+    # ensure tight regex match
+    prepare_command_regex = re.compile('^gobject prepare ([\da-f]+) ([\da-f]+) ([\d]+) ([\w-]+) ([\da-f]+)$')
+    cmd = outer_go.get_prepare_command()
+
+    match = prepare_command_regex.search(cmd)
+    assert match != None
+
+    # if match:
+    #     # print "matched!"
+    #     print(match.group(1))
+    #     print(match.group(2))
+    #     print(match.group(3))
+    #     print(match.group(4))
+    #     print(match.group(5))
+    # else:
+    #     print "NO MATCH!"
+
+    gobject_command = "gobject prepare 0 1 1471898632 chrono-trigger-party 5b5b2270726f706f73616c222c207b22656e645f65706f6368223a20313439313032323830302c2022676f7665726e616e63655f6f626a6563745f6964223a20352c20227061796d656e745f61646472657373223a2022795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e74223a20372e30303030303030302c202270726f706f73616c5f6e616d65223a20226368726f6e6f2d747269676765722d7061727479222c202273746172745f65706f6368223a20313438333235303430307d5d5d"
+    assert cmd == gobject_command
