45e0c4408180cc6b48840c75a891abce7350a020
Nathan Marley <nathan.marley@gmail.com>
2016-08-22 22:37:02 -0500

    tighten up DB a bit

--- database/001.sql
+++ database/001.sql
@@ -23,24 +23,26 @@ CREATE TABLE `governance_object` (
   `object_revision` int(20) NOT NULL DEFAULT '0',
   `object_data` text NOT NULL,
   `object_fee_tx` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_governance_object_object_name` (`object_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `proposal` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `governance_object_id` int(11) unsigned DEFAULT NULL,
-  `proposal_name` varchar(255) DEFAULT NULL,
+  `governance_object_id` int(11) unsigned NOT NULL,
+  `proposal_name` varchar(255) NOT NULL,
   `start_epoch` int(11) DEFAULT NULL,
   `end_epoch` int(11) DEFAULT NULL,
   `payment_address` varchar(255) DEFAULT NULL,
   `payment_amount` decimal(16,8) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `index_proposal_governance_object_id` (`governance_object_id`)
+  UNIQUE KEY `index_proposal_governance_object_id` (`governance_object_id`),
+  UNIQUE KEY `index_proposal_proposal_name` (`proposal_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE superblock (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `governance_object_id` int(11) unsigned DEFAULT NULL,
+  `governance_object_id` int(11) unsigned NOT NULL,
   `superblock_name` varchar(255) DEFAULT NULL,
   `event_block_height` int(11) DEFAULT NULL,
   `payment_addresses` text,
@@ -51,7 +53,7 @@ CREATE TABLE superblock (
 
 CREATE TABLE `event` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `governance_object_id` int(11) unsigned DEFAULT NULL,
+  `governance_object_id` int(11) unsigned NOT NULL,
   `start_time` int(11) NOT NULL DEFAULT '0',
   `prepare_time` int(11) NOT NULL DEFAULT '0',
   `submit_time` int(11) NOT NULL DEFAULT '0',
@@ -63,7 +65,7 @@ CREATE TABLE `event` (
 
 CREATE TABLE `action` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `governance_object_id` int(11) unsigned DEFAULT NULL,
+  `governance_object_id` int(11) unsigned NOT NULL,
   `absolute_yes_count` int DEFAULT NULL,
   `yes_count` int DEFAULT NULL,
   `no_count` int DEFAULT NULL,
