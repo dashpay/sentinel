782214e7b77d138918d5beaf1e7a3aa0b017ec0d
Nathan Marley <nathan.marley@gmail.com>
2016-08-24 12:03:49 -0500

    catch errors w/jsonrpc connection

--- sample.py
+++ sample.py
@@ -1,15 +1,20 @@
-
-
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from pprint import pprint
+import socket
 
 rpc_user = 'dashrpc'
 rpc_password = 'ZZZZZZsdrtyujk3498y7dcpoi3ue49orfjfpQKmXjzto'
 rpc_port = 19998
 
-# rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:%s"%(rpc_user, rpc_password, rpc_port))
-best_block_hash = rpc_connection.getbestblockhash()
-print(rpc_connection.getblock(best_block_hash))
+
+try:
+    best_block_hash = rpc_connection.getbestblockhash()
+    print(rpc_connection.getblockz(best_block_hash))
+except JSONRPCException as e:
+    print "JSONRPC Exception: %s" % e.message
+except socket.error as e:
+    print "Socket Error: %s" % e.strerror
 
 # batch support : print timestamps of blocks 0 to 99 in 2 RPC round-trips:
 #commands = [ [ "getblockhash", height] for height in range(100) ]
