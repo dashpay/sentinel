c8a614c819173787c09e387525b5ce45630430ad
Nathan Marley <nathan.marley@gmail.com>
2016-08-30 12:02:48 -0500

    stub fcns should use new interface too

--- scripts/blocknotify.py
+++ scripts/blocknotify.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 
 import json
-import dashd
+from dashd import DashDaemon
+from dashd import DashConfig
 
-import sys
-sys.path.append("lib")
+import sys, os
+sys.path.append( os.path.join( os.path.dirname(__file__), '..', 'lib' ) )
 
 from governance  import GovernanceObject
 
 """
-    - this script is ran ~2.5/minutes and processes updates to the governance system 
+    - this script is ran ~2.5/minutes and processes updates to the governance system
 
 """
 
-difflist = json.loads(dashd.cmd("governance diff"))
-for item in difflist:
-	obj = GovernanceObject(item)
-	obj.save()
+dashd = DashDaemon.from_dash_conf(config.dash_conf)
 
-	if obj.is_valid():
-		obj.vote(VOTE_ACTION_VALID, VOTE_OUTCOME_NO)
+difflist = json.loads(dashd.rpc_command("gobject diff"))
+# this currently isn't being used anyway
+# for item in difflist:
+    # NGM: TODO: ensure proper loading of these (need to test gobject diff output)
+	# obj = GovernanceObject(item)
+	# obj.save()
+    #
+	# if obj.is_valid():
+	# 	obj.vote(VOTE_ACTION_VALID, VOTE_OUTCOME_NO)
 pass
