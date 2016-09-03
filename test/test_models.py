1f22e961da373bac2f42cba542563879e19c2a00
Nathan Marley <nathan.marley@gmail.com>
2016-09-03 17:55:35 -0500

    fix test to include vote counts

--- test/test_models.py
+++ test/test_models.py
@@ -54,7 +54,8 @@ def test_governance_object(governance_object):
 
     fields = [ 'parent_id', 'object_creation_time', 'object_hash',
           'object_parent_hash', 'object_name', 'object_type', 'object_revision',
-          'object_fee_tx' ]
+          'object_fee_tx', 'yes_count', 'no_count', 'abstain_count',
+          'absolute_yes_count' ]
 
     fields.sort()
     sorted_keys = d.keys()
