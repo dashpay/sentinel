284af2c95fdb3b27bd69b953ecf410f319ae54f6
Nathan Marley <nathan.marley@gmail.com>
2016-09-16 11:47:25 -0500

    fix proposal tests

--- test/models/test_proposals.py
+++ test/models/test_proposals.py
@@ -7,10 +7,12 @@ import time
 os.environ['SENTINEL_ENV'] = 'test'
 sys.path.append( os.path.join( os.path.dirname(__file__), '..', 'lib' ) )
 sys.path.append( os.path.join( os.path.dirname(__file__), '..', '..', 'lib' ) )
+sys.path.append( os.path.join( os.path.dirname(__file__), '..' ) )
+sys.path.append( os.path.join( os.path.dirname(__file__), '..', '..' ) )
 
 import misc
+import config
 from models import GovernanceObject, Proposal
-
 
 # clear DB tables before each execution
 def setup():
@@ -43,24 +45,26 @@ def proposal():
 
 
 def test_proposal_is_valid(proposal):
+    from dashd import DashDaemon
+    dashd = DashDaemon.from_dash_conf(config.dash_conf)
 
     # fixture as-is should be valid
-    assert proposal.is_valid() == True
+    assert proposal.is_valid(dashd) == True
 
     # ============================================================
     # ensure end_date not greater than start_date
     # ============================================================
     proposal.end_epoch = proposal.start_epoch
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.end_epoch = proposal.start_epoch - 1
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.end_epoch = proposal.start_epoch + 0
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.end_epoch = proposal.start_epoch + 1
-    assert proposal.is_valid() == True
+    assert proposal.is_valid(dashd) == True
 
     # reset
     proposal.end_epoch = 1491022800
@@ -71,22 +75,22 @@ def test_proposal_is_valid(proposal):
     name = proposal.name
 
     proposal.name = '   heya!@209h '
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.name = "anything' OR 'x'='x"
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.name = ' '
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.name = ''
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.name = '0'
-    assert proposal.is_valid() == True
+    assert proposal.is_valid(dashd) == True
 
     proposal.name = 'R66-Y'
-    assert proposal.is_valid() == True
+    assert proposal.is_valid(dashd) == True
 
     # reset
     proposal.name = name
@@ -95,14 +99,14 @@ def test_proposal_is_valid(proposal):
     # ensure proposal not too late
     # ============================================================
     proposal.end_epoch = misc.get_epoch() - 1
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.end_epoch = misc.get_epoch()
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.start_epoch = misc.get_epoch() + 2
     proposal.end_epoch = misc.get_epoch() + 4
-    assert proposal.is_valid() == True
+    assert proposal.is_valid(dashd) == True
 
     # reset
     proposal.start_epoch = 1483250400
@@ -112,23 +116,23 @@ def test_proposal_is_valid(proposal):
     # ensure valid payment address
     # ============================================================
     proposal.payment_address = '7'
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.payment_address = 'YYE8KWYAUU5YSWSYMB3Q3RYX8XTUU9Y7UI'
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.payment_address = 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Uj'
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.payment_address = '221 B Baker St., London, United Kingdom'
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     # this is actually the Dash foundation multisig address...
     proposal.payment_address = '7gnwGHt17heGpG9Crfeh4KGpYNFugPhJdh'
-    assert proposal.is_valid() == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.payment_address = 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui'
-    assert proposal.is_valid() == True
+    assert proposal.is_valid(dashd) == True
 
 
     # ============================================================
@@ -139,10 +143,10 @@ def test_proposal_is_valid(proposal):
 
     # it's over 9000!
     proposal.payment_amount = 9001
-    assert proposal.is_valid(max_budget) == False
+    assert proposal.is_valid(dashd) == False
 
     proposal.payment_amount = -1
-    assert proposal.is_valid(max_budget) == False
+    assert proposal.is_valid(dashd) == False
 
 
 def test_proposal_is_deletable(proposal):
