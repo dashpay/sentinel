dacf68f40d7463870537e62e7c4541c16d803406
Nathan Marley <nathan.marley@gmail.com>
2016-09-19 20:01:08 -0500

    extensible config for possible future migration(s)

--- config.py
+++ config.py
@@ -25,6 +25,7 @@ def get_network():
     return sentinel_cfg.get('network', 'mainnet')
 
 def get_db_conn():
+    import peewee
     env = os.environ.get('SENTINEL_ENV', 'production')
 
     # default values should be used unless you need a different config for development
@@ -32,19 +33,29 @@ def get_db_conn():
     db_name     = sentinel_cfg.get('db_name', 'sentinel')
     db_user     = sentinel_cfg.get('db_user', 'sentinel')
     db_password = sentinel_cfg.get('db_password', 'sentinel')
-    db_charset  = sentinel_cfg.get('db_charset', 'utf8_bin')
+    db_charset  = sentinel_cfg.get('db_charset', 'utf8mb4')
+    db_driver   = sentinel_cfg.get('db_driver', 'mysql')
 
     if (env == 'test'):
         db_name = "%s_test" % db_name
 
+    peewee_drivers = {
+        'mysql': peewee.MySQLDatabase,
+        'postgres': peewee.PostgresqlDatabase,
+        'sqlite': peewee.SqliteDatabase,
+    }
+    driver = peewee_drivers.get(db_driver)
+
+    dbpfn = 'passwd' if db_driver == 'mysql' else 'password'
     db_conn = {
         'host': db_host,
         'user': db_user,
-        'passwd': db_password,
+        dbpfn: db_password,
     }
+    if driver == peewee.SqliteDatabase:
+        db_conn = {}
 
-    import peewee
-    db = peewee.MySQLDatabase(db_name, **db_conn)
+    db = driver(db_name, **db_conn)
 
     return db
 
