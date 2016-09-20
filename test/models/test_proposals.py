fdec6a29443c5f0e9ebc409012d406e9814c6f63
Nathan Marley <nathan.marley@gmail.com>
2016-09-20 13:46:21 -0500

    test proposal ordering

--- test/models/test_proposals.py
+++ test/models/test_proposals.py
@@ -12,18 +12,54 @@ sys.path.append( os.path.join( os.path.dirname(__file__), '..', '..' ) )
 
 import misc
 import config
-from models import GovernanceObject, Proposal
+from models import GovernanceObject, Proposal, Vote
 
 # clear DB tables before each execution
 def setup():
-    pass
     # clear tables first...
-    # Proposal.delete().execute()
-    # Superblock.delete().execute()
-    # GovernanceObject.delete().execute()
+    Vote.delete().execute()
+    Proposal.delete().execute()
+    GovernanceObject.delete().execute()
 
 def teardown():
     pass
+
+
+# list of govobjs to import for proposal ranking, ordering
+@pytest.fixture
+def golist():
+    items = [
+        {u'AbsoluteYesCount': 1000,
+         u'AbstainCount': 7,
+         u'CollateralHash': u'acb67ec3f3566c9b94a26b70b36c1f74a010a37c0950c22d683cc50da324fdca',
+         u'DataHex': u'5b5b2270726f706f73616c222c207b22656e645f65706f6368223a20313439313336383430302c20226e616d65223a20226465616e2d6d696c6c65722d35343933222c20227061796d656e745f61646472657373223a2022795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e74223a2032352e37352c202273746172745f65706f6368223a20313437343236313038362c202274797065223a20312c202275726c223a2022687474703a2f2f6461736863656e7472616c2e6f72672f6465616e2d6d696c6c65722d35343933227d5d5d',
+         u'DataString': u'[["proposal", {"end_epoch": 1491368400, "name": "dean-miller-5493", "payment_address": "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui", "payment_amount": 25.75, "start_epoch": 1474261086, "type": 1, "url": "http://dashcentral.org/dean-miller-5493"}]]',
+         u'Hash': u'dfd7d63979c0b62456b63d5fc5306dbec451180adee85876cbf5b28c69d1a86c',
+         u'IsValidReason': u'',
+         u'NoCount': 25,
+         u'YesCount': 1025,
+         u'fBlockchainValidity': True,
+         u'fCachedDelete': False,
+         u'fCachedEndorsed': False,
+         u'fCachedFunding': False,
+         u'fCachedValid': True},
+        {u'AbsoluteYesCount': 1000,
+         u'AbstainCount': 29,
+         u'CollateralHash': u'3efd23283aa98c2c33f80e4d9ed6f277d195b72547b6491f43280380f6aac810',
+         u'DataHex': u'5b5b2270726f706f73616c222c207b22656e645f65706f6368223a20313439313336383430302c20226e616d65223a20226665726e616e64657a2d37363235222c20227061796d656e745f61646472657373223a2022795965384b77796155753559737753596d4233713372797838585455753979375569222c20227061796d656e745f616d6f756e74223a2032352e37352c202273746172745f65706f6368223a20313437343236313038362c202274797065223a20312c202275726c223a2022687474703a2f2f6461736863656e7472616c2e6f72672f6665726e616e64657a2d37363235227d5d5d',
+         u'DataString': u'[["proposal", {"end_epoch": 1491368400, "name": "fernandez-7625", "payment_address": "yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui", "payment_amount": 25.75, "start_epoch": 1474261086, "type": 1, "url": "http://dashcentral.org/fernandez-7625"}]]',
+         u'Hash': u'0523445762025b2e01a2cd34f1d10f4816cf26ee1796167e5b029901e5873630',
+         u'IsValidReason': u'',
+         u'NoCount': 56,
+         u'YesCount': 1056,
+         u'fBlockchainValidity': True,
+         u'fCachedDelete': False,
+         u'fCachedEndorsed': False,
+         u'fCachedFunding': False,
+         u'fCachedValid': True},
+    ]
+
+    return items
 
 
 # Proposal
@@ -172,3 +208,22 @@ def test_proposal_is_deletable(proposal):
     # add a couple seconds for time variance
     proposal.end_epoch = now - ((86400 * 30) + 2)
     assert proposal.is_deletable() == True
+
+# deterministic ordering
+def test_approved_and_ranked(golist):
+    from dashd import DashDaemon
+    dashd = DashDaemon.from_dash_conf(config.dash_conf)
+    for item in golist:
+        (go, subobj) = GovernanceObject.import_gobject_from_dashd(dashd, item)
+
+    prop_list = Proposal.approved_and_ranked(dashd)
+
+    from pprint import pprint
+    # import pdb;pdb.set_trace()
+    # pprint(prop_list)
+
+    assert prop_list[0].object_hash == u'0523445762025b2e01a2cd34f1d10f4816cf26ee1796167e5b029901e5873630'
+    assert prop_list[1].object_hash == u'dfd7d63979c0b62456b63d5fc5306dbec451180adee85876cbf5b28c69d1a86c'
+
+
+
