9825a93d6e992b23f10afc2f1cd9707435e5e178
Andy Freer <andy@mightyhand.com>
2016-09-25 00:11:48 +0200

    db: Fix MySQL 5.5 compatibility on DB setup script (#6)

--- database/create-db.sql
+++ database/create-db.sql
@@ -7,8 +7,8 @@ CREATE DATABASE `sentinel` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
 CREATE DATABASE `sentinel_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
 
 # Create the user
-DROP USER IF EXISTS 'sentinel'@'localhost';
-SET @create_user = CONCAT("CREATE USER 'sentinel'@'localhost' IDENTIFIED BY '",@user_pwd,"';");
+SET @find_user=(SELECT COUNT(*) AS n FROM mysql.user WHERE User = 'sentinel' AND Host='localhost');
+SET @create_user = IF(@find_user > 0, 'SELECT 0', CONCAT("CREATE USER 'sentinel'@'localhost' IDENTIFIED BY '",@user_pwd,"';"));
 PREPARE stat FROM @create_user;
 EXECUTE stat;
 
