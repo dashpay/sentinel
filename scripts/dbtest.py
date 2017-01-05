ee2939edc4df3d0c293d5dcaed1d557be3035870
Nathan Marley <nathan.marley@gmail.com>
2017-01-05 06:41:40 -0800

    debug/test proposal window

--- scripts/dbtest.py
+++ scripts/dbtest.py
@@ -13,17 +13,18 @@ from dashd import DashDaemon
 import dashlib
 from decimal import Decimal
 dashd = DashDaemon.from_dash_conf(config.dash_conf)
+import misc
 # ==============================================================================
 # do shit here
 
-#pr = Proposal(
-#    name='proposal7',
-#    url='https://dashcentral.com/proposal7',
-#    payment_address='yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV',
-#    payment_amount=39.23,
-#    start_epoch=1483250400,
-#    end_epoch=1491022800,
-#)
+pr = Proposal(
+    name='proposal7',
+    url='https://dashcentral.com/proposal7',
+    payment_address='yTC62huR4YQEPn9AJHjnQxxreHSbgAoatV',
+    payment_amount=39.23,
+    start_epoch=1483250400,
+    end_epoch=1491022800,
+)
 
 #sb = Superblock(
 #    event_block_height = 62500,
@@ -31,8 +32,36 @@ dashd = DashDaemon.from_dash_conf(config.dash_conf)
 #    payment_amounts  = "5|3"
 #)
 
-pdb.set_trace()
+
+# TODO: make this a test, mock 'dashd' and tie a test block height to a
+# timestamp, ensure only unit testing a within_window method
+# 
+# also, create the `within_window` or similar method & use that.
+#
+bh = 131112
+bh_epoch = dashd.block_height_to_epoch(bh)
+
+fudge = 72000
+window_start = 1483689082 - fudge
+window_end = 1483753726 + fudge
+
+print("Window start: %s" % misc.epoch2str(window_start))
+print("Window end: %s" % misc.epoch2str(window_end))
+print("\nbh_epoch: %s" % misc.epoch2str(bh_epoch))
+
+
+if (bh_epoch < window_start or bh_epoch > window_end):
+    print("outside of window!")
+else:
+    print("Within window, we're good!")
+
+
+# pdb.set_trace()
+
+
 #dashd.get_object_list()
+
+
 
 # ==============================================================================
 #pdb.set_trace()
