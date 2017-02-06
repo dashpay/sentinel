85a6f9001cb9f1af0cc57ceec8e365251080ba99
moocowmoo <moo@dash.org>
2017-02-06 02:03:06 +0000

    enh - mainnet test mode

--- test/integration/test_jsonrpc.py
+++ test/integration/test_jsonrpc.py
@@ -14,8 +14,16 @@ from dash_config import DashConfig
 
 def test_dashd():
     config_text = DashConfig.slurp_config_file(config.dash_conf)
-    creds = DashConfig.get_rpc_creds(config_text, 'testnet')
+    network = 'mainnet'
+    is_testnet = False
+    genesis_hash = u'00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6'
+    for line in config_text.split("\n"):
+        if line.startswith('testnet=1'):
+            network = 'testnet'
+            is_testnet = True
+            genesis_hash = u'00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c'
 
+    creds = DashConfig.get_rpc_creds(config_text, network)
     dashd = DashDaemon(**creds)
     assert dashd.rpc_command is not None
 
@@ -24,7 +32,6 @@ def test_dashd():
     # Dash testnet block 0 hash == 00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c
     # test commands without arguments
     info = dashd.rpc_command('getinfo')
-
     info_keys = [
         'blocks',
         'connections',
@@ -38,7 +45,7 @@ def test_dashd():
     ]
     for key in info_keys:
         assert key in info
-    assert info['testnet'] is True
+    assert info['testnet'] is is_testnet
 
     # test commands with args
-    assert dashd.rpc_command('getblockhash', 0) == u'00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c'
+    assert dashd.rpc_command('getblockhash', 0) == genesis_hash
