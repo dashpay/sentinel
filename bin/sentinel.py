3f331ec7531b10e9be72f7885172802ef71e162b
Nathan Marley <nathan.marley@gmail.com>
2017-02-22 07:19:41 -0800

    remove delay

--- bin/sentinel.py
+++ bin/sentinel.py
@@ -146,12 +146,6 @@ def is_dashd_port_open(dashd):
     return port_open
 
 
-def delay():
-    delay_in_seconds = random.randint(1, 50)
-    printdbg("Delay of [%d] seconds to prevent watchdog sync issues")
-    time.sleep(delay_in_seconds)
-
-
 def main():
     dashd = DashDaemon.from_dash_conf(config.dash_conf)
 
@@ -177,7 +171,6 @@ def main():
         logger.setLevel(logging.DEBUG)
         logger.addHandler(logging.StreamHandler())
 
-    delay()
     # ========================================================================
     # general flow:
     # ========================================================================
