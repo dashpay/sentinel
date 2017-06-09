b16dbe6665ba263424b3505bd1ada118fbe6b0f7
sergey <sergey@dash.org>
2017-06-09 08:20:05 -0700

    empty line at the end of file

--- lib/dashd.py
+++ lib/dashd.py
@@ -242,4 +242,3 @@ class DashDaemon():
     def has_sentinel_ping(self):
         getinfo = self.rpc_command('getinfo')
         return (getinfo['protocolversion'] >= config.min_dashd_proto_version_with_sentinel_ping)
-
