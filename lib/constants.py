30b8b2a9f90433906f8251704e6e1d4f5cd854a0
Nathan Marley <nathan.marley@gmail.com>
2017-06-20 15:09:54 -0700

    constants

---
+++ lib/constants.py
@@ -0,0 +1,7 @@
+# for constants which need to be accessed by various parts of Sentinel
+
+# Sentinel version
+VERSION = '1.0.1'
+
+# skip proposals on superblock creation if the SB isn't within the fudge window
+SUPERBLOCK_FUDGE_WINDOW = 60 * 60 * 2
