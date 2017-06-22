12abf590bbd340a5c31d281361052db8547e4081
Nathan Marley <nathan.marley@gmail.com>
2017-06-21 18:08:54 -0700

    remove version from constants

--- lib/constants.py
+++ lib/constants.py
@@ -1,7 +1,4 @@
 # for constants which need to be accessed by various parts of Sentinel
 
-# Sentinel version
-VERSION = '1.0.1'
-
 # skip proposals on superblock creation if the SB isn't within the fudge window
 SUPERBLOCK_FUDGE_WINDOW = 60 * 60 * 2
