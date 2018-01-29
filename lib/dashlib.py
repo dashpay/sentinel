680a3fc72be85354da154af3ee7dd85fc050b3c7
jhodges10 <jeffhodges93@gmail.com>
2018-01-29 12:22:51 -0800

    Modified parse_raw_votes to only return masternode output, and skip the index.

--- lib/dashlib.py
+++ lib/dashlib.py
@@ -289,7 +289,7 @@ def parse_raw_votes(raw_votes):
 
         mn_collateral_outpoint = parse_masternode_status_vin(outpoint)
         v = {
-            'mn_collateral_outpoint': mn_collateral_outpoint,
+            'mn_collateral_outpoint': mn_collateral_outpoint.split('-')[0],
             'signal': signal,
             'outcome': outcome,
             'ntime': ntime,
