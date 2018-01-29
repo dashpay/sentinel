0b2af5fccb874d6d5be6ffc66c21e97b71555ad8
jhodges10 <jeffhodges93@gmail.com>
2018-01-29 12:20:06 -0800

    Added masternode_outpoint to Vote storage. (MIGHT WORK)

--- lib/models.py
+++ lib/models.py
@@ -223,7 +223,7 @@ class GovernanceObject(BaseModel):
                 continue
 
             printdbg('\tFound a matching valid vote on the network, outcome = %s' % vdikt['outcome'])
-            Vote(governance_object=self, signal=signal, outcome=outcome,
+            Vote(governance_object=self, masternode_outpoint = vote_info['mn_collateral_outpoint'], signal=signal, outcome=outcome,
                  object_hash=self.object_hash).save()
 
     def voted_on(self, **kwargs):
@@ -580,6 +580,7 @@ class Vote(BaseModel):
     governance_object = ForeignKeyField(GovernanceObject, related_name='votes', on_delete='CASCADE', on_update='CASCADE')
     signal = ForeignKeyField(Signal, related_name='votes', on_delete='CASCADE', on_update='CASCADE')
     outcome = ForeignKeyField(Outcome, related_name='votes', on_delete='CASCADE', on_update='CASCADE')
+    masternode_outpoint = CharField(max_length=64)
     voted_at = DateTimeField(default=datetime.datetime.utcnow())
     created_at = DateTimeField(default=datetime.datetime.utcnow())
     updated_at = DateTimeField(default=datetime.datetime.utcnow())
