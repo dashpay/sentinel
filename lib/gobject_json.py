9fdca48cc0d273114be3599a63dd4528cadeda88
Nathan Marley <nathan.marley@gmail.com>
2018-02-25 05:04:52 -0800

    implement new/old JSON object extraction method

--- lib/gobject_json.py
+++ lib/gobject_json.py
@@ -11,3 +11,23 @@ def valid_json(input):
         pass
 
     return is_valid
+
+
+def extract_object(json_input):
+    """
+    Given either an old-style or new-style Proposal JSON string, extract the
+    actual object used (ignore old-style multi-dimensional array and unused
+    string for object type)
+    """
+    if not valid_json(json_input):
+        raise Exception("Invalid JSON input.")
+
+    obj = simplejson.loads(json_input, use_decimal=True)
+
+    if (isinstance(obj, list) and
+        isinstance(obj[0], list) and
+        isinstance(obj[0][0], str) and
+        isinstance(obj[0][1], dict)):
+        obj = obj[0][1]
+
+    return obj
