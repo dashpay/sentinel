843ff3d7f1d14a556f7b53cd302c545d1503bedb
Nathan Marley <nathan.marley@gmail.com>
2018-03-29 16:50:41 -0700

    restrict proposals to 512 bytes

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -277,3 +277,13 @@ def test_approved_and_ranked(go_list_proposals):
 
     assert prop_list[0].object_hash == u'dfd7d63979c0b62456b63d5fc5306dbec451180adee85876cbf5b28c69d1a86c'
     assert prop_list[1].object_hash == u'0523445762025b2e01a2cd34f1d10f4816cf26ee1796167e5b029901e5873630'
+
+
+def test_proposal_size(proposal):
+    orig = Proposal(**proposal.get_dict())  # make a copy
+
+    # fixture as-is should be valid
+    assert proposal.is_valid() is True
+
+    proposal.url = 'https://' + ('x' * 513) + '.com/'
+    assert proposal.is_valid() is False
