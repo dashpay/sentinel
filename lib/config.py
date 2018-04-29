7ecfc7ea48ece1af5639a56c3d7c244f13de3fe0
Nathan Marley <nathan.marley@gmail.com>
2018-04-28 23:08:55 -0700

    bump minor version for release w/DashCore v12.3

--- lib/config.py
+++ lib/config.py
@@ -10,7 +10,7 @@ default_sentinel_config = os.path.normpath(
 )
 sentinel_config_file = os.environ.get('SENTINEL_CONFIG', default_sentinel_config)
 sentinel_cfg = DashConfig.tokenize(sentinel_config_file)
-sentinel_version = "1.1.0"
+sentinel_version = "1.2.0"
 min_dashd_proto_version_with_sentinel_ping = 70207
 
 
