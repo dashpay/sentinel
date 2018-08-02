f3d641fed5a80a04e437215e89c667c1c1e8513e
Nathan Marley <nathan.marley@gmail.com>
2018-08-01 21:33:01 -0700

    add test to cover DashConfig.slurp_config_file
    This ensures the method still works as expected after the raw string change in
    comment filter regex.

--- test/unit/test_dash_config.py
+++ test/unit/test_dash_config.py
@@ -62,7 +62,23 @@ def test_get_rpc_creds():
     assert creds.get('port') == 19998
 
 
-# ensure dash network (mainnet, testnet) matches that specified in config
-# requires running dashd on whatever port specified...
-#
-# This is more of a dashd/jsonrpc test than a config test...
+def test_slurp_config_file():
+    import tempfile
+
+    dash_config = """# basic settings
+#testnet=1 # TESTNET
+server=1
+printtoconsole=1
+txindex=1 # enable transaction index
+"""
+
+    expected_stripped_config = """server=1
+printtoconsole=1
+txindex=1 # enable transaction index
+"""
+
+    with tempfile.NamedTemporaryFile(mode='w') as temp:
+        temp.write(dash_config)
+        temp.flush()
+        conf = DashConfig.slurp_config_file(temp.name)
+        assert conf == expected_stripped_config
