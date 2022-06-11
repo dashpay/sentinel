d3c57fd0ed4a7868d100e78be9a604effb3fc9c2
Nathan Marley <nathan.marley@gmail.com>
2022-06-10 21:59:54 -0300

    Remove legacy MN outpoint parsing

--- lib/dashlib.py
+++ lib/dashlib.py
@@ -73,14 +73,8 @@ def elect_mn(**kwargs):
 
 
 def parse_masternode_status_vin(status_vin_string):
-    status_vin_string_regex = re.compile(r'CTxIn\(COutPoint\(([0-9a-zA-Z]+),\s*(\d+)\),')
-
-    m = status_vin_string_regex.match(status_vin_string)
-
-    # To Support additional format of string return from masternode status rpc.
-    if m is None:
-        status_output_string_regex = re.compile(r'([0-9a-zA-Z]+)-(\d+)')
-        m = status_output_string_regex.match(status_vin_string)
+    status_output_string_regex = re.compile(r'([0-9a-zA-Z]+)-(\d+)')
+    m = status_output_string_regex.match(status_vin_string)
 
     txid = m.group(1)
     index = m.group(2)
