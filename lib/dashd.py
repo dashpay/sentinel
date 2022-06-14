2cd1718652bcf17fb055be3a2cfd127298d72071
Nathan Marley <nathan.marley@gmail.com>
2022-06-14 18:10:01 -0300

    Get network from dashd, instead of sentinel config

--- lib/dashd.py
+++ lib/dashd.py
@@ -168,7 +168,7 @@ class DashDaemon():
     def is_govobj_maturity_phase(self):
         # 3-day period for govobj maturity
         maturity_phase_delta = 1662      # ~(60*24*3)/2.6
-        if config.network == 'testnet':
+        if self.network() == 'testnet':
             maturity_phase_delta = 24    # testnet
 
         event_block_height = self.next_superblock_height()
