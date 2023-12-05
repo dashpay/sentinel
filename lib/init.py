0d463a52f2a5557844ae485c1a73cbc966546a4c
thephez <thephez@users.noreply.github.com>
2023-12-05 12:47:28 -0500

    chore: don't exit after deprecation warning

--- lib/init.py
+++ lib/init.py
@@ -88,7 +88,6 @@ def has_required_env_vars():
 
 def main():
     print("deprecation warning: Dash Sentinel is deprecated and should be uninstalled. See https://github.com/dashpay/sentinel for details.")
-    sys.exit(1)
 
     install_instructions = "\tpip install -r requirements.txt"
 
